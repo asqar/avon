d81c411071095924728e12ccfb6db7ff5c201e0a
Askar Bakirov <b.asqar@gmail.com>
2019-07-30 03:54:57 +0200

    Oops

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import { MapPin } from 'react-feather'
 
-let mapkey = 'AIzaSyC6IwDk14O_G7kx_5ubE2_DnS5_B_jCGpc'
+let mapkey = ''
+if (process.env.NETLIFY_MAP_KEY) {
+  mapkey = process.env.NETLIFY_MAP_KEY
+}
 
 class GoogleMap extends Component {
   static defaultProps = {
